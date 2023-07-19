ba9bdb7f23c33a43cbf74b1481d093c9f651d260
amarok36 <bobrowww@mail.ru>
2023-07-19 19:57:59 +0500

    feat: database has been connected

---
+++ LibraryApp/DataBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace LibraryApp
+{
+    internal class DataBase
+    {
+        static SqliteConnection sqliteConnection = new("Data Source=mainDb.db");
+
+        public static void OpenConnection()
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Closed)
+                sqliteConnection.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Open)
+                sqliteConnection.Close();
+        }
+
+        public static SqliteConnection GetConnection() => sqliteConnection;
+    }
+}
